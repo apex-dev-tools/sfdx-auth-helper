b00fe761830dc73a480ce7aa79ab138909c69151
Kevin Jones <nawforce@gmail.com>
2022-07-09 23:16:50 +0100

    feat(test): add first test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,15 +1,20 @@
-/* 
-import { myPackage } from '../src';
+import { Messages } from '../src';
+import { getDefaultUsername } from '../src/Username';
 
-describe('index', () => {
-  describe('myPackage', () => {
-    it('should return a string containing the message', () => {
-      const message = 'Hello';
-
-      const result = myPackage(message);
-
-      expect(result).toMatch(message);
-    });
+describe('messages', () => {
+  it('auth messages are loaded', () => {
+    const messages = Messages.loadMessages('@salesforce/core', 'auth');
+    expect(messages).toBeDefined();
+    expect(messages.bundleName).toEqual('auth');
+    expect(messages.getMessage('defaultOrgNotFound', ['token'])).toEqual(
+      'No token org found'
+    );
   });
 });
-*/
+
+describe('default username', () => {
+  it('is undefined', async () => {
+    const username = await getDefaultUsername(process.cwd());
+    expect(username).toBeUndefined();
+  });
+});
